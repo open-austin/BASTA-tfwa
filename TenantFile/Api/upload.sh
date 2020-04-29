a5c9c55297e787b9a7bafc075073c13bb5e51004
Ryan Rightmer <rrightmer@gmail.com>
2020-04-29 18:33:57 -0500

    Missed a character

--- TenantFile/Api/upload.sh
+++ TenantFile/Api/upload.sh
@@ -2,4 +2,4 @@
 
 gcloud builds submit --tag gcr.io/tenant-file-fc6de/tenant-file-api
 
-gcloud run deploy tenant-file-app --image gcr.io/tenant-file-fc6de/tenant-file-api --platform managed --region=us-central1
+gcloud run deploy tenant-file-api --image gcr.io/tenant-file-fc6de/tenant-file-api --platform managed --region=us-central1
