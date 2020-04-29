a1f0b0b9a6b3543bbecfbc17eed935180c7c2179
Ryan Rightmer <rrightmer@gmail.com>
2020-04-29 18:30:21 -0500

    Fix deployment

--- TenantFile/Api/upload.sh
+++ TenantFile/Api/upload.sh
@@ -2,4 +2,4 @@
 
 gcloud builds submit --tag gcr.io/tenant-file-fc6de/tenant-file-api
 
-gcloud run deploy --image gcr.io/tenant-file-fc6de/tenant-file-api --platform managed
+gcloud run deploy tenant-file-app --image gcr.io/tenant-file-fc6de/tenant-file-api --platform managed --region=us-central1
