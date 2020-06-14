5dbdd0cae2df19c5f85b40cd2270b55a8328ed3e
hannahkamundson <hannahkamundson@gmail.com>
2020-06-14 14:11:13 -0500

    Beginning of thumbnail creation

--- TenantFile/Api/Services/GoogleCloudStorage.cs
+++ TenantFile/Api/Services/GoogleCloudStorage.cs
@@ -31,10 +31,22 @@ namespace TenantFile.Api.Services
 
         public async Task UploadToStorageAsync(string fileUrl, string filePath, string contentType)
         {
+            // Upload image to firebase
             WebClient webClient = new WebClient();
             var stream = webClient.OpenRead(fileUrl);
 
             await _storageClient.UploadObjectAsync(_bucketName, filePath, contentType, stream);
+
+            // Reshape the image to 128x128
+            // TODO Hannah how do I import??
+            Image originalImage = Image.FromStream(stream);
+            Image thumbNail = ImageResize.Crop(originalImage, 128, 128, ImageResize.AnchorPosition.Top);
+
+            // Uplaod the image to firebase
+            // TODO Hannah can I just put an image directly into OpenRead?
+            var thumbnailStream = webClient.OpenRead(thumbNail);
+            var thumbNailFilePath = "thumbnail/" + filePath;
+            await _storageClient.UploadObjectAsync(_bucketName, thumbNailFilePath, contentType, thumbnailStream);
         }
     }
 }
