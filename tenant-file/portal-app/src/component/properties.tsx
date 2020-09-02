008f370ee2502810866947cb9d6dcafa56f53d83
Chris Padilla <cdpadilla42@gmail.com>
2020-09-02 12:30:07 -0500

    Add Formik form forcreating and updating Properties

---
+++ tenant-file/portal-app/src/component/properties.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropertyForm from './property-form';
+import { useMutation, gql } from '@apollo/client';
+import { Container, Row, Col } from 'reactstrap';
+
+// TODO: Mutation to be implemented
+// const CREATE_PROPERTY = gql``;
+
+const Properties = () => {
+  // Below are dummy values that will later come from useMutation once Apollo is linked to the backend
+  let addProperty = (values: Object) => {
+    console.log('Creating new property', values);
+  };
+  let loading = false;
+  const error = null;
+
+  const isError = () => {
+    if (error) return true;
+    return false;
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>New Property</h1>
+          <PropertyForm
+            onSubmit={addProperty}
+            loading={loading}
+            error={isError()}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Properties;
