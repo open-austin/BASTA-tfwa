cd9e9beec777730e8cc89e6f2d687b75864acede
Chris Padilla <cdpadilla42@gmail.com>
2020-09-22 08:33:54 -0500

    Merge iss45 to use theme from styled componenets

---
+++ tenant-file/portal-app/src/component/export-toolbar.tsx
@@ -0,0 +1,19 @@
+import React, { MouseEvent } from 'react';
+import { NavLink } from 'react-router-dom';
+
+// type Props = {
+//   setIsSidebarOpen: (active: boolean) => void;
+//   renderLinks: () => React.ReactNode;
+// };
+
+const ExportToolbar = () => {
+  // Links to be displayed in main nav or mobile sidebar based on screen size
+
+  // const openSideBar = (e: React.MouseEvent) => {
+  //   setIsSidebarOpen(true);
+  // };
+
+  return <div className="export">Exporting your stuff!</div>;
+};
+
+export default ExportToolbar;
