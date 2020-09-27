a89ff49478ff62712b12057fbad0f05f17314613
Chris Padilla <cdpadilla42@gmail.com>
2020-09-26 19:25:09 -0500

    Refactor hamburger menu icon to a seperate svg file

--- tenant-file/portal-app/src/component/nav.tsx
+++ tenant-file/portal-app/src/component/nav.tsx
@@ -1,8 +1,8 @@
-import React, { MouseEvent } from "react";
-import StyledNav from "./styles/NavStyles";
-import AppLogoAndTitle from "./app-logo-and-title";
-import BarsIcon from "./bars-svg";
-import { NavLink } from "react-router-dom";
+import React, { MouseEvent } from 'react';
+import StyledNav from './styles/NavStyles';
+import AppLogoAndTitle from './app-logo-and-title';
+import { ReactComponent as BarsIcon } from '../images/bars.svg';
+import { NavLink } from 'react-router-dom';
 
 type Props = {
   setIsSidebarOpen: (active: boolean) => void;
