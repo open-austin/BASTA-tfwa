bc6f3eb3577bcc796600a216fe7ac98e610d5dab
Ryan Rightmer <rrightmer@gmail.com>
2020-10-04 14:35:58 -0500

    I like shadows

--- tenant-file/portal-app/src/component/footer.tsx
+++ tenant-file/portal-app/src/component/footer.tsx
@@ -5,7 +5,7 @@ import { RootState } from "../store/store";
 import { SignedInStatus } from "../store/auth";
 
 const StyledFooter = styled.footer`
-  border-top: 1px black solid;
+  box-shadow: 0 -1px 10px rgba(0, 0, 0, 0.3);
   background-color: ${(props) => props.theme.backdrop};
   padding: 0.5rem 1rem;
   color: ${(props) => props.theme.secondary};
