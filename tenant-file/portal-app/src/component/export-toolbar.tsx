bb40a02ad07b7915dff040a74dbf9b638d7c165d
Chris Padilla <cdpadilla42@gmail.com>
2020-10-19 14:26:10 -0500

    Add temporary onClick handling to export toolbar buttons

--- tenant-file/portal-app/src/component/export-toolbar.tsx
+++ tenant-file/portal-app/src/component/export-toolbar.tsx
@@ -6,15 +6,19 @@ type Props = {
 };
 
 const ExportToolbar = ({ isExportToolsOpen }: Props) => {
+  const handleClick = (e: React.MouseEvent) => {
+    console.log('click');
+  };
+
   return (
     <StyledExportToolbar className={isExportToolsOpen ? '' : 'hide'}>
-      <button tabIndex={0}>
+      <button onClick={handleClick}>
         <i className="fas fa-file-csv"></i> &#8594; CSV
       </button>
-      <button tabIndex={0}>
+      <button onClick={handleClick}>
         <i className="far fa-file-archive"></i> &#8594; ZIP
       </button>
-      <button tabIndex={0}>
+      <button onClick={handleClick}>
         <i className="fas fa-table"></i> &#8594; MAP
       </button>
     </StyledExportToolbar>
