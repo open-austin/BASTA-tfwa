02cf998f2cc0a3b42b792f49a8c854fc8b4cb01e
Hannah <48397717+hannahkamundson@users.noreply.github.com>
2020-10-24 12:05:48 -0500

    Making it so the weird error in firebase published code doesn't throw + made it so it automatically pushes to /dashboard on login + made it so it doesn't allow you to login again if you have already logged in (#80)
    Co-authored-by: Hannah Amundson <amundson.hannah@heb.com>

--- tenant-file/portal-app/src/store/store.ts
+++ tenant-file/portal-app/src/store/store.ts
@@ -1,5 +1,5 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { firebaseReducer } from "react-redux-firebase";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { firebaseReducer, actionTypes } from "react-redux-firebase";
 
 import authReducer from "./auth";
 
@@ -10,6 +10,13 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  // Weird bug and we are removing it by using this for now: https://github.com/prescottprue/react-redux-firebase/issues/761
+  // If you want to see if the bug is fixed, remove the belwo + go to the /login page and log in and see if an error occurs
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+        ignoredActions: [actionTypes.LOGIN, actionTypes.SET, actionTypes.SET_PROFILE]
+    }
+  }),
 });
 
 export type AppDispatch = typeof store.dispatch;
