8041007d8bf23cfeb35ec0b32e87760a02a96e87
Ryan Rightmer <rrightmer@gmail.com>
2020-10-27 10:54:21 -0500

    Remove comments (#84)

--- tenant-file/portal-app/src/component/tenant-list.tsx
+++ tenant-file/portal-app/src/component/tenant-list.tsx
@@ -120,61 +120,38 @@ const TenantList: React.FC = () => {
       <thead>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {
-              // Loop over the headers in each row
-              headerGroup.headers.map((column) => (
-                // Apply the header cell props
-                <th {...column.getHeaderProps()}>
-                  {
-                    // Render the header
-                    column.render("Header")
-                  }
-                </th>
-              ))
-            }
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {
-          // Loop over the table rows
-          rows.map((row) => {
-            // Prepare the row for display
-            prepareRow(row);
-            return (
-              // Apply the row props
-              <>
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {console.log(cell)}
-                          {
-                            // Render the cell contents
-                            cell.column.Header === "Images"
-                              ? cell.value.map((i: string) => (
-                                  <Image name={i} />
-                                ))
-                              : cell.render("Cell")
-                          }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <>
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {console.log(cell)}
+                      {cell.column.Header === "Images"
+                        ? cell.value.map((i: string) => <Image name={i} />)
+                        : cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
 
-                {/* <tr>
+              {/* <tr>
                   <td colSpan={2} className="text-center">
                     <Collapse>I'm Spanning Yo</Collapse>
                   </td>
                 </tr> */}
-              </>
-            );
-          })
-        }
+            </>
+          );
+        })}
       </tbody>
     </Table>
   );
