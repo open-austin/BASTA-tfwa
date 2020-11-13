c2c03cb97908d401dca0c4dcce72590e7508ad30
Chris Padilla <cdpadilla42@gmail.com>
2020-11-13 14:33:58 -0600

    Pull useHistory hook in for redirect on search submit

--- tenant-file/portal-app/src/component/name-search.tsx
+++ tenant-file/portal-app/src/component/name-search.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const NameSearch = () => {
   const [searchValue, setSearchValue] = useState('');
+  const history = useHistory();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setSearchValue(e.currentTarget.value);
   }
 
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(searchValue);
+    history.push(`/${searchValue}`);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search by name... "
