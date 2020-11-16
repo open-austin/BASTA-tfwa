ffbdece0ec91ded6299a3d3d64bffdf63f2d0363
Chris Padilla <cdpadilla42@gmail.com>
2020-11-16 11:08:36 -0600

    Revert "Add line awesome as npm package"
    Revert to iss44

--- tenant-file/portal-app/src/App.tsx
+++ tenant-file/portal-app/src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Login from './component/login';
-import Layout from './component/layout';
-import PrivateRoute from './component/private-route';
-import DisplayImages from './component/display-images';
-import Admin from './component/admin';
-import Dashboard from './component/dashboard';
-import Properties from './component/properties';
-import Home from './component/home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./component/login";
+import Layout from "./component/layout";
+import PrivateRoute from "./component/private-route";
+import DisplayImages from "./component/display-images";
+import Admin from "./component/admin";
+import Dashboard from "./component/dashboard";
+import Properties from "./component/properties";
+import Home from "./component/home";
 
 function App() {
   return (
