e7a43eed0002e96bafe938a60fa3b22de7031dba
rixong <rixong@gmail.com>
2020-11-25 16:27:31 -0600

    tenant-details page made

--- tenant-file/portal-app/src/component/dashboard.tsx
+++ tenant-file/portal-app/src/component/dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TenantList from './tenant-list';
+import TenantDetails from './tenant-details';
 import { Container } from 'reactstrap';
 
 const Dashboard: React.FC = () => (
   <Container>
     <TenantList />
+    {/* <TenantDetails /> */}
   </Container>
 );
 
---
+++ tenant-file/portal-app/src/component/tenant-details.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TenantDetails: React.FC = () => {
+  return (
+    <div>
+      <h4>Tenant Details - placeholder </h4>
+    </div>
+  )
+}
+
+export default TenantDetails;
