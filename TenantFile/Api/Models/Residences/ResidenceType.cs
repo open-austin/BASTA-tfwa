55a6938b4b7e2a5d904cb2e19c50d4c8e1c55a15
James Bock <jamesbock10@gmail.com>
2020-12-01 11:54:37 -0500

    fix for GetAddress on ResidenceType

--- TenantFile/Api/Models/Residences/ResidenceType.cs
+++ TenantFile/Api/Models/Residences/ResidenceType.cs
@@ -38,13 +38,13 @@ namespace TenantFile.Api.Models.Residences
            AddressByIdDataLoader dataLoader,
            CancellationToken cancellationToken)
         {
-            var addressIds = context.Residences.AsQueryable()//don't use include...add AddresssId to Entity...Address is the Princpal for Property, Residence and Complex
-               .Where(p => p.Id == residence.Id)
-               .Select(r => r.AddressId)
-               .Single();//Could return Address here BUT I believe fetching the ID then passing them all to the Dataloader to make one call to the DB is the benefit ofthe dataloader? n+1?
+            //var addressIds = context.Residences.AsQueryable()//don't use include...add AddresssId to Entity...Address is the Princpal for Property, Residence and Complex
+            //   .Where(p => p.Id == residence.Id)
+            //   .Select(r => r.AddressId)
+            //   .Single();//Could return Address here BUT I believe fetching the ID then passing them all to the Dataloader to make one call to the DB is the benefit ofthe dataloader? n+1?
 
 
-            return dataLoader.LoadAsync(addressIds, cancellationToken);
+            return dataLoader.LoadAsync(residence.AddressId, cancellationToken);
 
         }
     }
