309fa49db440db8f33fbe6c37ced4038b9e7cd62
James Bock <jamesbock10@gmail.com>
2020-12-03 16:28:31 -0500

    Removed Premise entity

---
+++ TenantFile/Api/DataLoader/DataLoaderFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenDonut;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TenantFile.Api.Common;
+using TenantFile.Api.Models;
+
+namespace TenantFile.Api.DataLoader
+{
+    public interface IDataLoaderFactory
+    {
+        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service) where T : class, IEntity;
+    }
+
+    public class DataLoaderFactory : IDataLoaderFactory
+    {
+        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service) where T : class, IEntity
+        {
+            var ctx = service.GetRequiredService<IDbContextFactory<TenantFileContext>>();
+
+            var dbSetInfo = ctx;
+           
+            return new DataLoaderById<T>(service.GetRequiredService<IBatchScheduler>(),
+                                         ctx,
+                                         func => GetDbSet<T>(func));
+        }
+        public DbSet<T> GetDbSet<T>(TenantFileContext context) where T : class, IEntity
+        {
+            {
+                var dbSetInfo = context.GetType().GetProperties().Where(c => c.PropertyType == typeof(DbSet<T>)).FirstOrDefault()!;
+
+                return (DbSet<T>)context.GetType().InvokeMember(
+                    dbSetInfo.Name,
+                    System.Reflection.BindingFlags.GetProperty,
+                    binder: default,
+                    target: context,
+                    null)!;
+            }
+        }
+    }
+}
+
