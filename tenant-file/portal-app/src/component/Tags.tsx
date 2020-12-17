7a41174209dd3794874cd2b29e4e4da4b076e2e1
Chris Padilla <cdpadilla42@gmail.com>
2020-12-17 15:51:51 -0600

    Write initial, ugly styling to tags display

--- tenant-file/portal-app/src/component/Tags.tsx
+++ tenant-file/portal-app/src/component/Tags.tsx
@@ -1,7 +1,94 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledTagsDisplay = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid grey;
+  display: flex;
+  border-radius: 10px;
+
+  .display,
+  .header,
+  .body,
+  .flex-row {
+    width: 100%;
+  }
+
+  .header,
+  .flex-row {
+    padding: 0.5rem;
+  }
+
+  .header {
+    display: flex;
+
+    & > * {
+      flex: 1;
+    }
+
+    .sort {
+      text-align: right;
+    }
+  }
+
+  .flex-row {
+    display: flex;
+
+    & > * {
+      flex: 1;
+    }
+  }
+
+  .buttons {
+    text-align: right;
+  }
+`;
 
 const Tags = () => {
-  return <div>You're it!</div>;
+  const data = [
+    {
+      name: 'Chris',
+      description: 'Good guy',
+      photoCount: 5,
+    },
+    {
+      name: 'Jenn',
+      description: 'cool gal',
+      photoCount: 4,
+    },
+    {
+      name: 'Elvis',
+      description: 'Big guy',
+      photoCount: 5,
+    },
+  ];
+
+  return (
+    <StyledTagsDisplay>
+      <div className="display">
+        <div className="header">
+          <div className="title">12 labels</div>
+          <div className="sort">
+            <button>sort</button>
+          </div>
+        </div>
+        <div className="body">
+          {data.map((tag) => (
+            <div className="flex-row">
+              <div className="label">{tag.name}</div>
+              <div className="description">{tag.description}</div>
+              <div className="photoCount">{tag.photoCount}</div>
+              <div className="buttons">
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </StyledTagsDisplay>
+  );
 };
 
 export default Tags;
