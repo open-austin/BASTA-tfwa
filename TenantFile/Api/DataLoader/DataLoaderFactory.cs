86f82dab880988f2baacfd245f885d499645f80e
James Bock <jamesbock10@gmail.com>
2020-12-18 15:45:08 -0500

    Generic Dataloader with Factory w/o Reflection

--- TenantFile/Api/DataLoader/DataLoaderFactory.cs
+++ TenantFile/Api/DataLoader/DataLoaderFactory.cs
@@ -14,7 +14,7 @@ namespace TenantFile.Api.DataLoader
     public interface IDataLoaderFactory/*<T> where T : class, IEntity*/
     {
         //Func<IDbContextFactory<TenantFileContext>, DbSet<T>> dbSetFunc { get; init; }
-        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service) where T : class, IEntity;
+        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service, Func<TenantFileContext, DbSet<T>> func) where T : class, IEntity;
        // {
             
         // Type dbSetType { get; init; }
@@ -29,37 +29,13 @@ namespace TenantFile.Api.DataLoader
         //public IEntity[] dbSetType { get; init; } = null!;
         // public Func<IDbContextFactory<TenantFileContext>, DbSet<T>> dbSetFunc { get ; init; }
 
-        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service) where T : class, IEntity
+        public DataLoaderById<T> CreateDataLoader<T>(IServiceProvider service, Func<TenantFileContext,DbSet<T>>  func) where T : class, IEntity
         {
             var ctx = service.GetRequiredService<IDbContextFactory<TenantFileContext>>();
 
-            var dbSetInfo = ctx;
-            //return new DataLoaderById<T>(default!, ctx,
-            //    func => (DbSet<T>)func
-            //    .GetType().InvokeMember(
-            //    dbSetInfo.Name,
-            //    System.Reflection.BindingFlags.GetProperty,
-            //    binder: default,
-            //    target: func,
-            //    null)!);  
+            
             return new DataLoaderById<T>(service.GetRequiredService<IBatchScheduler>(), ctx,
-                func => GetDbSet<T>(func));
-        }
-        public DbSet<T> GetDbSet<T>(TenantFileContext context) where T : class, IEntity
-        {
-            {
-                //var t = dbSetType.GetType();
-
-
-                var dbSetInfo = context.GetType().GetProperties().Where(c => c.PropertyType == typeof(DbSet<T>)).FirstOrDefault()!;
-
-                return (DbSet<T>)context.GetType().InvokeMember(
-                    dbSetInfo.Name,
-                    System.Reflection.BindingFlags.GetProperty,
-                    binder: default,
-                    target: context,
-                    null)!;
-            }
+                func);
         }
     }
 }
