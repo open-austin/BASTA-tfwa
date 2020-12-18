aa7601ae056fed1116e7624d0b38031dde86d9ba
Chris Padilla <cdpadilla42@gmail.com>
2020-12-18 10:19:23 -0600

    Finishing touches to colors, accents, and spacing

--- tenant-file/portal-app/src/component/Tags.tsx
+++ tenant-file/portal-app/src/component/Tags.tsx
@@ -5,9 +5,10 @@ const StyledTagsDisplay = styled.div`
   width: 95%;
   max-width: 1200px;
   margin: 0 auto;
-  border: 1px solid grey;
+  border: 1px solid rgba(0, 0, 0, 0.3);
   display: flex;
   border-radius: 10px;
+  box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.1);
 
   .display,
   .header,
@@ -20,11 +21,11 @@ const StyledTagsDisplay = styled.div`
   .flex-row {
     padding: 0.5rem;
     padding: 1rem;
-    border-bottom: 1px solid grey;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
   }
 
   .header {
-    background-color: lightgray;
+    background-color: rgba(0, 0, 0, 0.08);
     border-radius: 8px 8px 0 0;
   }
 
@@ -47,6 +48,7 @@ const StyledTagsDisplay = styled.div`
   .flex-row {
     display: flex;
     justify-content: center;
+    font-size: 0.8rem;
 
     & > * {
       flex: 1;
@@ -54,7 +56,7 @@ const StyledTagsDisplay = styled.div`
       align-items: center;
     }
 
-    .label span {
+    .label button {
       /* TODO Fix stretch */
       display: inline-block;
       padding: 0.1rem 0.5rem;
@@ -62,13 +64,13 @@ const StyledTagsDisplay = styled.div`
       border-radius: 15px;
       font-weight: 700;
       color: #eee;
-      font-size: 0.8rem;
+      box-shadow: none;
+      border: none;
     }
   }
 
   .body .flex-row:last-child {
     border-bottom: none;
-    background-color: rgba(10, 100, 10, 0.5);
     border-radius: 0 0 8px 8px;
   }
 
@@ -81,7 +83,22 @@ const StyledTagsDisplay = styled.div`
       background-color: ${(props) => props.theme.backdrop};
       color: ${(props) => props.theme.primary};
       border-radius: 8px;
-      padding: 0 0.6rem;
+      padding: 0.3rem 0.8rem;
+      font-size: 0.8rem;
+      transition: color 0.4s ease;
+    }
+
+    button:hover {
+      color: ${(props) => props.theme.accent};
+    }
+
+    button:first-child {
+      border-radius: 8px 0 0 8px;
+      border-right: 1px solid white;
+    }
+
+    button:last-child {
+      border-radius: 0 8px 8px 0;
     }
   }
 `;
@@ -110,15 +127,13 @@ const Tags = () => {
       <div className="display">
         <div className="header">
           <div className="title">12 labels</div>
-          <div className="sort">
-            <button>sort</button>
-          </div>
+          <div className="sort">{/* <button>sort</button> */}</div>
         </div>
         <div className="body">
           {data.map((tag) => (
             <div className="flex-row">
               <div className="label">
-                <span>{tag.name}</span>
+                <button>{tag.name}</button>
               </div>
               <div className="description">{tag.description}</div>
               <div className="photoCount">{tag.photoCount}</div>
