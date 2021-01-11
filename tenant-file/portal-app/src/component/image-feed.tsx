f0b76ae9acaef0c9ddf601b0284d978a58ea60f6
Chris Padilla <cdpadilla42@gmail.com>
2021-01-11 16:30:15 -0600

    Fetch sample data

--- tenant-file/portal-app/src/component/image-feed.tsx
+++ tenant-file/portal-app/src/component/image-feed.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ImageFeed = () => {
-  return <div>Images here!</div>;
+  const [images, setImages] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function getImages() {
+      const blob = await fetch('https://picsum.photos/v2/list');
+      const result = await blob.json();
+      console.log(result);
+      setImages(result);
+    }
+    getImages();
+  }, []);
+
+  return <div>{images.map((image) => image?.url)}</div>;
 };
 
 export default ImageFeed;
