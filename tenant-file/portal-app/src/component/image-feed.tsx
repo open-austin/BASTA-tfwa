3c78fb703cf65b2856bede021e30d0e96d540144
Chris Padilla <cdpadilla42@gmail.com>
2021-01-12 21:18:32 -0600

    Layout sorting function

--- tenant-file/portal-app/src/component/image-feed.tsx
+++ tenant-file/portal-app/src/component/image-feed.tsx
@@ -21,9 +21,21 @@ const StyledImageFeed = styled.div`
   }
 `;
 
-const ImageFeed = () => {
-  const [images, setImages] = useState<any[]>([]);
+// Type for sample data from Lorem Picsum, can be scrapped later
+interface Image {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
 
+const ImageFeed = () => {
+  // TODO: correct type - when pulling from back end, set type to string[]
+  const [images, setImages] = useState<Image[]>([]);
+  const [sortField, setSortField] = useState('');
+  const [sortOrder, setSortOrder] = useState(1);
   useEffect(() => {
     async function getImages() {
       const blob = await fetch('https://picsum.photos/v2/list');
@@ -34,8 +46,23 @@ const ImageFeed = () => {
     getImages();
   }, []);
 
+  function sortImages(field: keyof Image, order: number) {
+    // Correct types here when incorperating images from back end
+    const sortedImages = [...images].sort((a: Image, b: Image) => {
+      if (a[field] > b[field]) {
+        return 1;
+      } else if (a[field] < b[field]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    setImages(sortedImages);
+  }
+
   return (
     <StyledImageFeed>
+      <button onClick={() => sortImages('author', 1)}>Sort</button>
       {images.map((image) => {
         return (
           <div className="image_container">
