6658764d85b08e037b95407b9da1e77259a71da6
Chris Padilla <cdpadilla42@gmail.com>
2021-01-17 16:09:14 -0600

    Style and add form inputs form inputs

--- tenant-file/portal-app/src/component/image-feed.tsx
+++ tenant-file/portal-app/src/component/image-feed.tsx
@@ -2,13 +2,35 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const StyledImageFeed = styled.div`
-  display: grid;
-  gap: 0.5rem;
   width: 90%;
   max-width: 1600px;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  justify-content: center;
   margin: 0 auto;
+
+  .change_order {
+    position: relative;
+    width: 2rem;
+    align-self: stretch;
+    font-size: 1.1rem;
+    background-color: rgba(0, 0, 0, 0);
+    border: none;
+    & i:first-child {
+      position: absolute;
+      top: 0;
+    }
+    & i:last-child {
+      position: relative;
+      transform: translateY(0.3rem);
+    }
+  }
+
+  .grid {
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    justify-content: center;
+    margin: 0 auto;
+    width: 100%;
+  }
 
   .image_container {
     display: flex;
@@ -62,27 +84,49 @@ const ImageFeed = () => {
 
   return (
     <StyledImageFeed>
-      <button
-        onClick={() => {
-          setSortField('author');
-        }}
-      >
-        Sort
-      </button>
-      <button
-        onClick={() => {
-          setSortOrder(sortOrder * -1);
-        }}
-      >
-        Order
-      </button>
-      {sortImages().map((image) => {
-        return (
-          <div className="image_container">
-            <img src={image.download_url} alt="" style={{ width: '100%' }} />
-          </div>
-        );
-      })}
+      <div className="filters">
+        <label htmlFor="sort_field">Sort by:</label>
+        <select
+          name="sort_field"
+          onChange={() => {
+            setSortField('author');
+          }}
+        >
+          <option value="Date Added" selected>
+            Date Added
+          </option>
+          <option value="id">Id</option>
+          <option value="name">Name</option>
+        </select>
+        <button className="change_order">
+          <i className="las la-angle-up"></i>
+          <i className="las la-angle-down"></i>
+        </button>
+
+        <button
+          onClick={() => {
+            setSortField('author');
+          }}
+        >
+          Sort
+        </button>
+        <button
+          onClick={() => {
+            setSortOrder(sortOrder * -1);
+          }}
+        >
+          Order
+        </button>
+      </div>
+      <div className="grid">
+        {sortImages().map((image) => {
+          return (
+            <div className="image_container">
+              <img src={image.download_url} alt="" style={{ width: '100%' }} />
+            </div>
+          );
+        })}
+      </div>
     </StyledImageFeed>
   );
 };
