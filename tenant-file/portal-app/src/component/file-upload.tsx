582b91709907135d38df8ff87d5087115f3ced56
Chris Padilla <cdpadilla42@gmail.com>
2021-01-29 10:59:56 -0600

    Style and implement drag and drop with react-dropzone

--- tenant-file/portal-app/src/component/file-upload.tsx
+++ tenant-file/portal-app/src/component/file-upload.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+const StyledFileUpload = styled.div`
+  border: 8px dashed grey;
+  width: 80%;
+  margin: 0 auto;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  &.active {
+    border: 8px dashed ${(props) => props.theme.darkSecondary};
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+
+  .las {
+    font-size: 5rem;
+  }
+`;
 
 const FileUpload = () => {
-  return <div>Gimme your files!! D:</div>;
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <>
+      <StyledFileUpload
+        className={isDragActive ? 'active' : ''}
+        {...getRootProps()}
+      >
+        <input {...getInputProps()} />
+        <i className="las la-cloud-upload-alt"></i>
+        <p>Drag & Drop or Click to Upload</p>
+      </StyledFileUpload>
+    </>
+  );
 };
 
 export default FileUpload;
