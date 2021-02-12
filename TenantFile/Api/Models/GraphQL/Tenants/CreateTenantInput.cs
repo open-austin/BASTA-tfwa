21f73d77eb5223dd79eef483d0c272a6879f0e49
JamesBock <jamesbock10@gmail.com>
2021-02-11 20:02:12 -0600

    Applying Unique ieds in all models

--- TenantFile/Api/Models/GraphQL/Tenants/CreateTenantInput.cs
+++ TenantFile/Api/Models/GraphQL/Tenants/CreateTenantInput.cs
@@ -1,10 +1,37 @@
-﻿using TenantFile.Api.Models.Residences;
+﻿using HotChocolate.Types.Relay;
+using TenantFile.Api.Models.Entities;
+using TenantFile.Api.Models.Residences;
 
 namespace TenantFile.Api.Models.Tenants
 {
-  public record CreateTenantInput(
-      string Name,
-      string PhoneNumber,
-      CreateResidenceInput? CurrentResidence
-      );
+    public record CreateTenantInput(
+        string Name,
+        string PhoneNumber,
+        CreateResidenceInput? CurrentResidence,
+        [ID(nameof(Residence))] int? ResidenceId //this can be handled by the Residence Input if reconfigured
+        ){}
+    // {
+    //     public CreateTenantInput(string Name,
+    //     string PhoneNumber,
+    //     CreateResidenceInput? CurrentResidence) : this(Name, PhoneNumber, CurrentResidence, null)
+    //     {
+    //     }
+    //     public CreateTenantInput(string Name,
+    //     string PhoneNumber,
+    //     [ID(nameof(Residence))] int? ResidenceId) : this(Name, PhoneNumber, null, ResidenceId)
+    //     {
+    //     }
+    //     public void Deconstruct(out string name, out string phoneNumber, out CreateResidenceInput? currentResidence)
+    //     {
+    //         name = Name;
+    //         phoneNumber= PhoneNumber;
+    //         currentResidence = CurrentResidence;
+    //     }
+    //     public void Deconstruct(out string name, out string phoneNumber,[ID(nameof(Residence))] out int? residenceId)
+    //     {
+    //         name = Name;
+    //         phoneNumber= PhoneNumber;
+    //         residenceId = ResidenceId;
+    //     }
+    // }
 }
