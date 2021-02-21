1ed5f655630159194c163c4a31b6e6a42ab53c99
Pat Min <patmin@Pats-MacBook-Pro.local>
2021-02-20 18:34:48 -0700

    Got Texas to show as the default state

--- tenant-file/portal-app/src/component/add-property.tsx
+++ tenant-file/portal-app/src/component/add-property.tsx
@@ -9,8 +9,13 @@ import { Input } from "reactstrap";
 import { fieldNameFromStoreName } from "@apollo/client/cache/inmemory/helpers";
 import { stringify } from "querystring";
 import { Console } from "console";
+//import SelectUSState from "react-select-us-states";
+import Select from "react-select";
 
 // Credit to the documentation for GraphQL, Apollo, and Formik
+// Credit to the "react-select" package: https://react-select.com/home
+// Credit to this list of states + DC: https://gist.github.com/pusherman/3145761
+
 
 const ADD_PROPERTY = gql`
     mutation addingAProperty($fullName: String!, $phoneNumber: String!, $houseNumber: Int!, $street: String!, $city: String!, $zip: Int!, $bldgName: String!) {
@@ -40,7 +45,7 @@ export default () => {
             addrLn3: '',
             addrLn4: '',
             city: '',
-            state: '',
+            state: 'TX',
             zip: '',
             bldgName: ''
         }}
@@ -91,7 +96,59 @@ export default () => {
                 <Field id="city" name="city" />
                 <br></br>
                 <label style={{textIndent: '127px'}} htmlFor="state">*State:</label>
-                <Field id="state" name="state" />
+                <Field as="select" id="state" name="state">
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </Field>
                 <br></br>
                 <label style={{textIndent: '99px'}} htmlFor="zip">*Zip Code:</label>
                 <Field id="zip" name="zip" />
