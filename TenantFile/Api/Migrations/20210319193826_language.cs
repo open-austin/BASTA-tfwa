98e6bc0665111f3bf2cb77f1f6dd6ef8017a214b
James Bock <jamesbock10@gmail.com>
2021-05-11 18:30:53 -0500

    Fix unused var and add migrations

---
+++ TenantFile/Api/Migrations/20210319193826_language.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TenantFile.Api.Migrations
+{
+    public partial class language : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredLanuage",
+                table: "Phones",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferredLanuage",
+                table: "Phones");
+        }
+    }
+}
--- tenant-file/portal-app/src/component/add-property.tsx
+++ tenant-file/portal-app/src/component/add-property.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import ReactDOM from "react-dom";
 
 import { Formik, Field, Form } from "formik";
 
